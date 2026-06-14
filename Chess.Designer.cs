cf72b0903b19dcc3fb3bafa97f2039c92a507ccb
JackMagee21 <mt.j07@yahoo.com>
2026-06-15 00:37:46 +0100

    Add project files.

---
+++ Chess.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Chess
+{
+    partial class Chess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_8A = new System.Windows.Forms.Panel();
+            this.panel_7A = new System.Windows.Forms.Panel();
+            this.panel_6A = new System.Windows.Forms.Panel();
+            this.panel_5A = new System.Windows.Forms.Panel();
+            this.panel_4A = new System.Windows.Forms.Panel();
+            this.panel_3A = new System.Windows.Forms.Panel();
+            this.panel_2A = new System.Windows.Forms.Panel();
+            this.panel_1A = new System.Windows.Forms.Panel();
+            this.panel_2B = new System.Windows.Forms.Panel();
+            this.panel_3B = new System.Windows.Forms.Panel();
+            this.panel_4B = new System.Windows.Forms.Panel();
+            this.panel_5B = new System.Windows.Forms.Panel();
+            this.panel_6B = new System.Windows.Forms.Panel();
+            this.panel_7B = new System.Windows.Forms.Panel();
+            this.panel_8B = new System.Windows.Forms.Panel();
+            this.panel_1B = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_8A
+            // 
+            this.panel_8A.BackColor = System.Drawing.Color.White;
+            this.panel_8A.Location = new System.Drawing.Point(0, 0);
+            this.panel_8A.Name = "panel_8A";
+            this.panel_8A.Size = new System.Drawing.Size(100, 100);
+            this.panel_8A.TabIndex = 0;
+            // 
+            // panel_7A
+            // 
+            this.panel_7A.BackColor = System.Drawing.Color.Black;
+            this.panel_7A.Location = new System.Drawing.Point(0, 100);
+            this.panel_7A.Name = "panel_7A";
+            this.panel_7A.Size = new System.Drawing.Size(100, 100);
+            this.panel_7A.TabIndex = 1;
+            // 
+            // panel_6A
+            // 
+            this.panel_6A.BackColor = System.Drawing.Color.White;
+            this.panel_6A.Location = new System.Drawing.Point(0, 200);
+            this.panel_6A.Name = "panel_6A";
+            this.panel_6A.Size = new System.Drawing.Size(100, 100);
+            this.panel_6A.TabIndex = 1;
+            // 
+            // panel_5A
+            // 
+            this.panel_5A.BackColor = System.Drawing.Color.Black;
+            this.panel_5A.Location = new System.Drawing.Point(0, 300);
+            this.panel_5A.Name = "panel_5A";
+            this.panel_5A.Size = new System.Drawing.Size(100, 100);
+            this.panel_5A.TabIndex = 2;
+            // 
+            // panel_4A
+            // 
+            this.panel_4A.BackColor = System.Drawing.Color.White;
+            this.panel_4A.Location = new System.Drawing.Point(0, 400);
+            this.panel_4A.Name = "panel_4A";
+            this.panel_4A.Size = new System.Drawing.Size(100, 100);
+            this.panel_4A.TabIndex = 2;
+            // 
+            // panel_3A
+            // 
+            this.panel_3A.BackColor = System.Drawing.Color.Black;
+            this.panel_3A.Location = new System.Drawing.Point(0, 500);
+            this.panel_3A.Name = "panel_3A";
+            this.panel_3A.Size = new System.Drawing.Size(100, 100);
+            this.panel_3A.TabIndex = 3;
+            // 
+            // panel_2A
+            // 
+            this.panel_2A.BackColor = System.Drawing.Color.White;
+            this.panel_2A.Location = new System.Drawing.Point(0, 600);
+            this.panel_2A.Name = "panel_2A";
+            this.panel_2A.Size = new System.Drawing.Size(100, 100);
+            this.panel_2A.TabIndex = 3;
+            // 
+            // panel_1A
+            // 
+            this.panel_1A.BackColor = System.Drawing.Color.Black;
+            this.panel_1A.Location = new System.Drawing.Point(0, 700);
+            this.panel_1A.Name = "panel_1A";
+            this.panel_1A.Size = new System.Drawing.Size(100, 100);
+            this.panel_1A.TabIndex = 4;
+            // 
+            // panel_2B
+            // 
+            this.panel_2B.BackColor = System.Drawing.Color.Black;
+            this.panel_2B.Location = new System.Drawing.Point(100, 600);
+            this.panel_2B.Name = "panel_2B";
+            this.panel_2B.Size = new System.Drawing.Size(100, 100);
+            this.panel_2B.TabIndex = 12;
+            // 
+            // panel_3B
+            // 
+            this.panel_3B.BackColor = System.Drawing.Color.White;
+            this.panel_3B.Location = new System.Drawing.Point(100, 500);
+            this.panel_3B.Name = "panel_3B";
+            this.panel_3B.Size = new System.Drawing.Size(100, 100);
+            this.panel_3B.TabIndex = 10;
+            // 
+            // panel_4B
+            // 
+            this.panel_4B.BackColor = System.Drawing.Color.Black;
+            this.panel_4B.Location = new System.Drawing.Point(100, 400);
+            this.panel_4B.Name = "panel_4B";
+            this.panel_4B.Size = new System.Drawing.Size(100, 100);
+            this.panel_4B.TabIndex = 11;
+            // 
+            // panel_5B
+            // 
+            this.panel_5B.BackColor = System.Drawing.Color.White;
+            this.panel_5B.Location = new System.Drawing.Point(100, 300);
+            this.panel_5B.Name = "panel_5B";
+            this.panel_5B.Size = new System.Drawing.Size(100, 100);
+            this.panel_5B.TabIndex = 8;
+            // 
+            // panel_6B
+            // 
+            this.panel_6B.BackColor = System.Drawing.Color.Black;
+            this.panel_6B.Location = new System.Drawing.Point(100, 200);
+            this.panel_6B.Name = "panel_6B";
+            this.panel_6B.Size = new System.Drawing.Size(100, 100);
+            this.panel_6B.TabIndex = 9;
+            // 
+            // panel_7B
+            // 
+            this.panel_7B.BackColor = System.Drawing.Color.White;
+            this.panel_7B.Location = new System.Drawing.Point(100, 100);
+            this.panel_7B.Name = "panel_7B";
+            this.panel_7B.Size = new System.Drawing.Size(100, 100);
+            this.panel_7B.TabIndex = 6;
+            // 
+            // panel_8B
+            // 
+            this.panel_8B.BackColor = System.Drawing.Color.Black;
+            this.panel_8B.Location = new System.Drawing.Point(100, 0);
+            this.panel_8B.Name = "panel_8B";
+            this.panel_8B.Size = new System.Drawing.Size(100, 100);
+            this.panel_8B.TabIndex = 7;
+            // 
+            // panel_1B
+            // 
+            this.panel_1B.BackColor = System.Drawing.Color.White;
+            this.panel_1B.Location = new System.Drawing.Point(100, 700);
+            this.panel_1B.Name = "panel_1B";
+            this.panel_1B.Size = new System.Drawing.Size(100, 100);
+            this.panel_1B.TabIndex = 5;
+            // 
+            // Chess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 803);
+            this.Controls.Add(this.panel_1B);
+            this.Controls.Add(this.panel_2B);
+            this.Controls.Add(this.panel_1A);
+            this.Controls.Add(this.panel_3B);
+            this.Controls.Add(this.panel_2A);
+            this.Controls.Add(this.panel_4B);
+            this.Controls.Add(this.panel_3A);
+            this.Controls.Add(this.panel_5B);
+            this.Controls.Add(this.panel_4A);
+            this.Controls.Add(this.panel_6B);
+            this.Controls.Add(this.panel_5A);
+            this.Controls.Add(this.panel_7B);
+            this.Controls.Add(this.panel_6A);
+            this.Controls.Add(this.panel_8B);
+            this.Controls.Add(this.panel_7A);
+            this.Controls.Add(this.panel_8A);
+            this.Name = "Chess";
+            this.Text = "Chess 1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_8A;
+        private System.Windows.Forms.Panel panel_7A;
+        private System.Windows.Forms.Panel panel_6A;
+        private System.Windows.Forms.Panel panel_5A;
+        private System.Windows.Forms.Panel panel_4A;
+        private System.Windows.Forms.Panel panel_3A;
+        private System.Windows.Forms.Panel panel_2A;
+        private System.Windows.Forms.Panel panel_1A;
+        private System.Windows.Forms.Panel panel_2B;
+        private System.Windows.Forms.Panel panel_3B;
+        private System.Windows.Forms.Panel panel_4B;
+        private System.Windows.Forms.Panel panel_5B;
+        private System.Windows.Forms.Panel panel_6B;
+        private System.Windows.Forms.Panel panel_7B;
+        private System.Windows.Forms.Panel panel_8B;
+        private System.Windows.Forms.Panel panel_1B;
+    }
+}
+
